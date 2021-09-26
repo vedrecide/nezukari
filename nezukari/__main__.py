231f31fb5c0c64df304c422da9e4e2bc4568869b
1olipop <mohammedrayyan112233@gmail.com>
2021-09-26 19:17:42 +0530

    Loading Tanjun Bot :D

--- nezukari/__main__.py
+++ nezukari/__main__.py
@@ -1,5 +1,10 @@
 from nezukari.core.bot import Bot
+from nezukari.core.client import Client
 
 if __name__ == '__main__':
     bot = Bot()
+    (
+        Client.from_gateway_bot(bot)
+        ._load_modules()
+    )
     bot.run()

35b0d5153dbea67075b9a7dc9a2c1247be25933f
1olipop <mohammedrayyan112233@gmail.com>
2021-09-26 21:24:40 +0530

    Added pause component

---
+++ nezukari/components/pause.py
@@ -0,0 +1,18 @@
+import tanjun
+
+pause_component = tanjun.Component()
+
+@pause_component.with_slash_command
+@tanjun.as_slash_command("pause", "Pause the current song being played")
+async def pause(ctx: tanjun.abc.Contex) -> None:
+    node = await ctx.shards.data.lavalink.get_guild_node(ctx.guild_id)
+    
+    if not node or not node.now_playing:
+        return await ctx.respond("Nothing is being played at the moment")
+
+    await ctx.shards.data.lavalink.pause(ctx.guild_id)
+    await ctx.respond(f"Paused successfully")
+
+@tanjun.as_loader
+def load(client: tanjun.Client) -> None:
+    client.add_component(pause_component.copy())

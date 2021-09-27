3cdf1ea3419381fc72b3269965410c98bf55a6b4
1olipop <mohammedrayyan112233@gmail.com>
2021-09-27 06:39:25 +0530

    Fixed volume command

--- nezukari/components/volume.py
+++ nezukari/components/volume.py
@@ -1,7 +1,4 @@
 import tanjun
-import hikari
-
-from typing import Union
 
 volume_component = tanjun.Component()
 
@@ -9,17 +6,17 @@ volume_component = tanjun.Component()
 @volume_component.with_slash_command
 @tanjun.with_int_slash_option("volume", "Volume to be set (Between 0 and 100)")
 @tanjun.as_slash_command("volume", "Increase/Decrease the volume")
-async def volume(ctx: tanjun.abc.Context, volume: int) -> Union[hikari.Message, None]:
+async def volume(ctx: tanjun.abc.Context, volume: int) -> None:
     node = await ctx.shards.data.lavalink.get_guild_node(ctx.guild_id)
 
     if not node or not node.now_playing:
         return await ctx.respond("Nothing is being played at the moment")
 
     if 0 < volume < 100:
-        return await ctx.respond("Volume should be between 0 and 100")
-
-    await ctx.shards.data.lavalink.volume(ctx.guild_id, volume)
-    await ctx.respond(f"Set the volume to {volume}")
+        await ctx.shards.data.lavalink.volume(ctx.guild_id, volume)
+        await ctx.respond(f"Set the volume to {volume}")
+    else:
+        await ctx.respond("Volume should be between 0 and 100")
 
 
 @tanjun.as_loader
